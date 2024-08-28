3b05d08b4ae8009b64d4324619ae3f34715410c7
CEJSH <eun2763@naver.com>
2024-08-28 17:46:19 +0900

    Add: 삭제하기(+requestSubmit)

---
+++ src/components/review-item-delete-button.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { deleteReviewAction } from "@/actions/delete-review.action";
+import { useActionState, useEffect, useRef } from "react";
+
+export default function ReviewItemDeleteButton({
+  reviewId,
+  bookId,
+}: {
+  reviewId: number;
+  bookId: number;
+}) {
+  // submit메서드는 유효성검사나 이벤트 핸들러 다 무시하고 무조건 강제로 폼에 제출시키는 위험한 메서드..(주의해 사용해야함)
+  // requestSubmit 비교적 의도한대로 작동할 가능성이 크다
+  const formRef = useRef<HTMLFormElement>(null);
+  const [state, formAction, isPending] = useActionState(
+    deleteReviewAction,
+    null
+  );
+
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+
+  return (
+    <form ref={formRef} action={formAction}>
+      <input name="reviewId" value={reviewId} hidden />
+      <input name="bookId" value={bookId} hidden />
+      {isPending ? (
+        <div>...</div>
+      ) : (
+        <div onClick={() => formRef.current?.requestSubmit()}>삭제하기</div>
+      )}
+    </form>
+  );
+}
