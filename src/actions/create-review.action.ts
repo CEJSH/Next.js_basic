6e11b6a96f7f409d15881275aedea4aaba03f4b9
CEJSH <eun2763@naver.com>
2024-08-28 17:20:11 +0900

    Add: useActionState 추가

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,17 +1,22 @@
 "use server";
 
+import { delay } from "@/util/delay";
 import { revalidatePath, revalidateTag } from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
   const bookId = formData.get("bookId")?.toString();
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요.",
+    };
   }
 
   try {
+    await delay(2000);
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       {
@@ -33,8 +38,18 @@ export async function createReviewAction(formData: FormData) {
     // revalidatePath(`/`, "layout");
 
     // 5. 태그 기준. 데이터 캐시 재검증
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     revalidateTag(`review=${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${err}`,
+    };
   }
 }
